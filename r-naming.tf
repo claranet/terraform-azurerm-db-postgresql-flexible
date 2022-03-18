636a70829238f3c47206ecb565a4d5381f7092a9
Alan Boutilly <alan.boutilly@fr.clara.net>
2022-03-18 11:41:13 +0100

    AZ-602 - Edit azurecaf suffix name

--- r-naming.tf
+++ r-naming.tf
@@ -2,7 +2,7 @@ resource "azurecaf_name" "postgresql_flexible_server" {
   name          = var.stack
   resource_type = "azurerm_postgresql_flexible_server"
   prefixes      = var.name_prefix == "" ? null : [local.name_prefix]
-  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, var.use_caf_naming ? "" : "postgresql"])
+  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, var.use_caf_naming ? "" : "psqlf"])
   use_slug      = var.use_caf_naming
   clean_input   = true
   separator     = "-"
@@ -14,7 +14,7 @@ resource "azurecaf_name" "postgresql_flexible_dbs" {
   name          = var.stack
   resource_type = "azurerm_postgresql_flexible_server_database"
   prefixes      = var.name_prefix == "" ? null : [local.name_prefix]
-  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, each.value, var.use_caf_naming ? "" : "postgresql"])
+  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, each.value, var.use_caf_naming ? "" : "psqlf"])
   use_slug      = var.use_caf_naming
   clean_input   = true
   separator     = "-"
