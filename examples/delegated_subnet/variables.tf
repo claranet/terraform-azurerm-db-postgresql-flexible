85ffd556a3999f32873cb1630fef41bfabb3042f
Sébastien Caloone <sebastien.caloone@fr.clara.net>
2022-12-09 12:15:19 +0100

    AZ-933 adding pre_condition on private_dns_zone_id and delegated_subnet_id vars

---
+++ examples/delegated_subnet/variables.tf
@@ -0,0 +1,39 @@
+variable "azure_region" {
+  description = "Azure region to use."
+  type        = string
+}
+
+variable "client_name" {
+  description = "Client name/account used in naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Project environment"
+  type        = string
+}
+
+variable "stack" {
+  description = "Project stack name"
+  type        = string
+}
+
+variable "vnet_cidr" {
+  description = "cidr for the vnet"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "cidr for the subnet"
+  type        = string
+}
+
+variable "administrator_login" {
+  description = "Administrator login for PostgreSQL server"
+  type        = string
+}
+
+variable "administrator_password" {
+  description = "Administrator password for PostgreSQL server"
+  type        = string
+}
